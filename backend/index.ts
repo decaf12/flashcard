6f17a5b963dd01fcd2576114b7b2897a90a7890b
Siyang He <siyang.he.uw@gmail.com>
2023-12-28 23:41:24 -0500

    server starts

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,27 @@
+import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import app from './server';
 
 dotenv.config();
 
 const port = process.env.PORT ?? 8000;
+
+if (process.env.CONNECTION_STRING !== null && process.env.CONNECTION_STRING !== undefined) {
+  MongoClient.connect(
+    process.env.CONNECTION_STRING,
+    {
+      maxPoolSize: 50,
+      waitQueueTimeoutMS: 2500,
+    }
+  ).then(async (client: MongoClient) => {
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}.`);
+    });
+  }).catch((err: Error) => {
+    console.error(err.message);
+    console.error(err.stack);
+    process.exit(1);
+  });
+} else {
+  console.error('Missing connection string in .env.');
+}
--- backend/server.ts
+++ backend/server.ts
@@ -5,4 +5,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 export default app;
